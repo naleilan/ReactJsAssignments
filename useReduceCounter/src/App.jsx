01632ac9d001302de32b1de817fd372ef1c80e5c
naleilan <leilan.n@gmail.com>
2024-02-02 10:14:08 +0300

    add

--- useReduceCounter/src/App.jsx
+++ useReduceCounter/src/App.jsx
@@ -1,4 +1,15 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "inc":
+      return { count: state.count + 1 };
+    case "dec":
+      return { count: state.count - 1 };
+    default:
+      throw new Error("unknown action");
+  }
+}
 
 export default function App() {
   return (
@@ -9,60 +20,23 @@ export default function App() {
 }
 
 function Counter() {
-  const [isOpen, setIsOpen] = useState(true);
-  const [step, setStep] = useState(1);
-  const [count, setCount] = useState(0);
-  const date = new Date();
-  date.setDate(date.getDate() + count);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
-  function handlePrvStep() {
-    if (step > 1) setStep(s => s - 1);
-    console.log(step);
+  function handleDec() {
+    dispatch({ type: "dec" });
   }
 
-  function handleNextStep() {
-    if (step < 100) setStep(s => s + 1);
-    console.log(step);
-  }
-
-  function handlePrvCount() {
-    if (count > 1) setCount(c => c - step);
-  }
-
-  function handleNextCount() {
-    if (count < 100) setCount(c => c + step);
+  function handleInc() {
+    dispatch({ type: "inc" });
   }
 
   return (
-    <div>
-      <button className="close" onClick={() => setIsOpen(is => !is)}>
-        &times;
-      </button>
-      {isOpen && (
-        <div className="counter">
-          <div className="step">
-            <button onClick={handlePrvStep}>&#45;</button>
-            <h3>Step: {step} </h3>
-            <button onClick={handleNextStep}>&#43;</button>
-          </div>
-          <div className="count">
-            <button onClick={handlePrvCount}>&#45;</button>
-            <h3>Count: {count}</h3>
-            <button onClick={handleNextCount}>&#43;</button>
-          </div>
-          <div className="msg">
-            <span>
-              {' '}
-              {count === 0
-                ? 'Today is '
-                : count > 0
-                ? `${count} days from today is `
-                : `${Math.abs(count)} days ago was `}
-            </span>
-            <span>{date.toDateString()}</span>
-          </div>
-        </div>
-      )}
+    <div className="counter">
+      <div className="count">
+        <button onClick={handleDec}>&#45;</button>
+        <h3>Count: {state.count}</h3>
+        <button onClick={handleInc}>&#43;</button>
+      </div>
     </div>
   );
 }
