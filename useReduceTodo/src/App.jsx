b07b04890e22ccfca4cd242ad158eebf2ea82261
naleilan <leilan.n@gmail.com>
2024-02-02 13:09:40 +0300

    add

--- useReduceTodo/src/App.jsx
+++ useReduceTodo/src/App.jsx
@@ -1,42 +1,37 @@
 import React, { useReducer, useState } from "react";
 
+const ACTIONS = {
+  TODO_ADD: "add",
+};
+
 function reducer(state, action) {
   switch (action.type) {
-    case "inc":
-      return { count: state.count + 1 };
-    case "dec":
-      return { count: state.count - 1 };
-    default:
-      throw new Error("unknown action");
+    case ACTIONS.TODO_ADD:
+      return [...state, newTodo(action.payload.name)];
   }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
 }
 
 export default function App() {
-  return (
-    <>
-      <Counter />
-    </>
-  );
-}
+  const [state, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState("");
 
-function Counter() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-
-  function handleDec() {
-    dispatch({ type: "dec" });
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.TODO_ADD, payload: { name: name } });
+    setName("");
   }
-
-  function handleInc() {
-    dispatch({ type: "inc" });
-  }
-
+  console.log(state);
   return (
-    <div className="counter">
-      <div className="count">
-        <button onClick={handleDec}>&#45;</button>
-        <h3>Count: {state.count}</h3>
-        <button onClick={handleInc}>&#43;</button>
-      </div>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    </form>
   );
 }
